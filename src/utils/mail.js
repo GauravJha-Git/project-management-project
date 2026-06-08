b8ca502379daddf4e5241e1cbb726c6018652fd8
GauravJha-Git <gauravjha092006@gmail.com>
2026-06-09 03:56:26 +0530

    feat: add mailgen integration for email verification and password reset templates

---
+++ src/utils/mail.js
@@ -0,0 +1,48 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "Welcome to our App! We're excited to have you onboard.",
+
+            action: {
+                instructions: "To verify your email, please click on the following button:",
+
+                button: {
+                    color: "#22BC66",
+                    text: "Verify your email",
+                    link: verificationUrl,
+                },
+            },
+
+            outro: "Need help? Just reply to this email.",
+        },
+    };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "we got a request to reset password of your account",
+
+            action: {
+                instructions: "to reset password click on the following button or link",
+
+                button: {
+                    color: "#22BC66",
+                    text: "Reset Password",
+                    link: passwordResetUrl,
+                },
+            },
+
+            outro: "Need help? Just reply to this email.",
+        },
+    };
+};
+
+export {
+    emailVerificationMailgenContent ,
+    forgotPasswordMailgenContent
+}
