c978cc6f210b7d5259ceb9c8edd3e16d03013896
GauravJha-Git <gauravjha092006@gmail.com>
2026-06-08 22:15:54 +0530

    feat: restructure project and implement user model with authentication
    - Updated package.json to change entry point to src/index.js and added new dependencies (bcrypt, cors, express, jsonwebtoken, mongoose).
    - Created .gitkeep file in public/images directory.
    - Implemented app.js to set up Express server with CORS and basic configurations.
    - Added health check controller and route for server status.
    - Established MongoDB connection logic in db/index.js.
    - Developed user model with password hashing, JWT generation, and token management.
    - Created utility functions for API error and response handling.
    - Added async handler for error management in asynchronous routes.
    - Defined constants for user roles and task statuses.

---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,23 @@
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handeler.js";
+
+/*
+const healthCheck = (req,res)=>{
+    try{
+        res
+        .status(200)
+        .json(
+            new ApiResponse(200,{message:"server is running"})
+        );
+    }catch(error){}
+};
+*/
+
+
+
+const healthCheck = asyncHandler(async(req,res)=>{
+    res.status(200).json(
+        new ApiResponse(200, {message :"Server is running"})
+    );
+});
+export {healthCheck};
