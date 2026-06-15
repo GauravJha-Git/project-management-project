6e4f810e39d7941ad72a6e7a31e407b0fe09e093
GauravJha-Git <gauravjha092006@gmail.com>
2026-06-15 07:09:54 +0530

    feat: implement logout functionality and update auth routes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handeler.js";
-import jwt from jsonwebtoken;
+import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
@@ -10,8 +10,8 @@ export const verifyJWT = asyncHandler(async(req,res,next)=>{
     }
 
     try {
-        const decodedToken = jwt.verify(token,process.env.ACESS_TOKEN_SECRET);
-        const user = await User.findbyId(decodedToken?._id).select("-password -refreshToken -emailverificationToken -emailVerificationExpiry");
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken -emailverificationToken -emailVerificationExpiry");
         if(!user){
         throw new ApiError(401,"invalid access Token");
     }
